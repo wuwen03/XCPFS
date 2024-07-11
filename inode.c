c7a1bc459eb78ad83021e5154d43fc5fa7c81e5e
wuwen <1056374449@qq.com>
2024-07-11 21:43:04 +0800

    update

--- inode.c
+++ inode.c
@@ -1,5 +1,20 @@
 #include"xcpfs.h"
 
 struct inode *xcpfs_iget(struct super_block *sb, nid_t ino) {
+    struct inode *inode;
+    struct xcpfs_node *node;
+    struct xcpfs_inode *ri;
+    struct page *page;
+    inode = iget_locked(sb,ino);
+    if(!inode) {
+        return PTR_ERR(-ENOMEM);
+    }
+    if(!(inode->i_state & I_NEW)) {
+        return inode;
+    }
 
+    page = get_node_page(sb,ino,false);
+    node = (struct xcpfs_node *)page_address(page);
+    ri = node->i;
+    //TODO
 }
--- super.c
+++ super.c
@@ -1,15 +1,45 @@
 #include"xcpfs.h"
 
 static struct inode *xcpfs_alloc_inode(struct super_block *sb) {
+    struct xcpfs_inode_info *xi;
+    xi = kzalloc(sizeof(struct xcpfs_inode_info),GFP_KERNEL);
+    if(!xi) {
+        return NULL;
+    }
+    inode_init_once(&xi->vfs_inode);
+
+    return &xi->vfs_inode;
+}
+
+static void xcpfs_destroy_inode(struct inode *inode) {
+    struct xcpfs_inode_info *xi = (struct xcpfs_inode_info *)inode;
+    kfree(xi);
+}
+
+static void xcpfs_drop_inode(struct inode *inode) {
+    return generic_drop_inode(inode);
+}
+//TODO
+static void xcpfs_evict_inode(struct inode *inode) {
+    struct xcpfs_inode_info *si = (struct xcpfs_inode_info *)inode;
 
 }
 
-static void xcpfs_free_inode(struct inode *) {
+static void xcpfs_put_super(struct super_block* sb) {
+    struct xcpfs_sb_info *sbi = sb->s_fs_info;
 
+
+}
+
+static int xcpfs_statfs(struct dentry* dentry, struct kstatfs* buf) {
+    return -EIO;
 }
 
 const struct super_operations sfs_sops = {
     .alloc_inode = xcpfs_alloc_inode,
-    .free_inode = xcpfs_free_inode,
-    
+    .destroy_inode = xcpfs_destroy_inode,
+    .drop_inode = xcpfs_drop_inode,
+    .evict_inode = xcpfs_evict_inode,
+    .put_super = xcpfs_put_super,
+    .statfs = xcpfs_statfs,
 }
